1c0ca10761937192f206a3aacaac6f83378f02e4
tiagofeitosa <tiagofeitosa89@hotmail.com>
2018-04-15 19:04:26 -0300

    Revisao 3

--- index.php
+++ index.php
@@ -12,26 +12,52 @@
     <link href="css/style.css" rel="stylesheet" type="text/css">
     <link href="css/carousel.css" rel="stylesheet" type="text/css">
 
+    <script>
+        function myFunction() {
+            var x = document.getElementById("myTopnav");
+            if (x.className === "topnav") {
+                x.className += " responsive";
+            } else {
+                x.className = "topnav";
+            }
+        }
+
+        window.onscroll = function() {scrollFunction()};
+
+        function scrollFunction() {
+            if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+                document.getElementById("myBtn").style.display = "block";
+            } else {
+                document.getElementById("myBtn").style.display = "none";
+            }
+        }
+
+        // When the user clicks on the button, scroll to the top of the document
+        function topFunction() {
+            document.body.scrollTop = 0; // For Safari
+            document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+        }
+
+    </script>
+
 
 </head>
 <body>
 
 <div class="container-fluid" id="app">
 
-    <div class="row">
-        <div class="col-md-12" id="titulo-site">
-            <h3 class="col-md-2 col-xs-12">Fut Histórias</h3>
-            <h5 class="col-md-9 col-xs-12">Tudo que você quer saber sobre as seleções da copa da Rússia e não sabe onde encontrar</h5>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-md-12" id="menu">
-            <ul  class="col-xs-12">
-                <li><a href="#">Home</a> </li>
-                <li><a href="selecoes.php">Seleções</a> </li>
-                <li><a href="info.php">Informações e curiosidades</a> </li>
-                <li><a href="fasegrupos.php">Fase de grupos</a> </li>
-            </ul>
+    <div class="row" id="linha-titulo-site">
+        <div class="container">
+            <div class="col-md-6 col-xs-6" id="titulo-site"><!--título site-->
+                <h3>Fut Histórias</h3>
+            </div>
+            <div class="topnav" id="myTopnav">
+                <a href="index.php">Home</a>
+                <a href="selecoes.php">Seleções</a>
+                <a href="fasegrupos.php">Fase de grupos</a>
+                <a href="info.php">Informações e curiosidades</a>
+                <a href="javascript:void(0);" class="icon" onclick="myFunction()">Menu</a>
+            </div>
         </div>
     </div>
 
@@ -109,70 +135,59 @@
 
     <div class="row">
         <div id="estadios-copa" class="col-md-12">
-            <div id="desc-estadios-copa" class="col-md-6 col-xs-12">
-                <h2>CONHEÇA UM POUCO SOBRE OS ESTÁDIOS QUE SEDIARÃO JOGOS DA COPA DE 2018</h2>
-
-                <h5>Clique no nome do estádio desejado</h5>
+            <div class="container">
+                <div id="desc-estadios-copa" class="col-md-6 col-xs-12">
+                    <h2>CONHEÇA UM POUCO SOBRE OS ESTÁDIOS QUE SEDIARÃO JOGOS DA COPA DE 2018</h2>
+                    <h5>Clique no nome do estádio desejado</h5>
+                </div>
+                <div id="imagem-estadios-copa" class="post-content">
+                    <img src="img/estadios.png">
+                </div>
             </div>
-            <div id="imagem-estadios-copa" class="post-content">
-                <img src="img/estadios.png">
-            </div>
-
         </div>
-    </div>
+    </div><!--Sobre estadios-->
 
     <div class="row">
-        <div id="noticias" class="col-md-12">
-            <h3>NOTÍCIAS DA COPA</h3><hr>
-            <div class="row">
-                <div class="col-md-3">
-                    <img src="img/noticias/image-1.jpg" width="100%" height="100%">
-                    <p class="selecao-noticia">SELEÇÃO</p>
-                    <p class="conteudo-noticia">Kroos: ‘O Brasil nos mostrou que não somos tão bons quanto dizem’</p>
-                </div>
+        <div class="container">
+            <div id="noticias" class="col-md-12">
+                <h3>NOTÍCIAS DA COPA</h3><hr>
+                <div class="row">
+                    <div class="col-md-3">
+                        <img src="img/noticias/image-1.jpg" width="100%" height="100%">
+                        <p class="selecao-noticia">SELEÇÃO</p>
+                        <p class="conteudo-noticia">Kroos: ‘O Brasil nos mostrou que não somos tão bons quanto dizem’</p>
+                    </div>
 
-                <div class="col-md-3">
-                    <img src="img/noticias/image-1.jpg" width="100%" height="100%">
-                    <p class="selecao-noticia">SELEÇÃO</p>
-                    <p class="conteudo-noticia">Kroos: ‘O Brasil nos mostrou que não somos tão bons quanto dizem’</p>
-                </div>
+                    <div class="col-md-3">
+                        <img src="img/noticias/image-1.jpg" width="100%" height="100%">
+                        <p class="selecao-noticia">SELEÇÃO</p>
+                        <p class="conteudo-noticia">Kroos: ‘O Brasil nos mostrou que não somos tão bons quanto dizem’</p>
+                    </div>
 
-                <div class="col-md-3">
-                    <img src="img/noticias/image-1.jpg" width="100%" height="100%">
-                    <p class="selecao-noticia">SELEÇÃO</p>
-                    <p class="conteudo-noticia">Kroos: ‘O Brasil nos mostrou que não somos tão bons quanto dizem’</p>
-                </div>
+                    <div class="col-md-3">
+                        <img src="img/noticias/image-1.jpg" width="100%" height="100%">
+                        <p class="selecao-noticia">SELEÇÃO</p>
+                        <p class="conteudo-noticia">Kroos: ‘O Brasil nos mostrou que não somos tão bons quanto dizem’</p>
+                    </div>
 
-                <div class="col-md-3">
-                    <img src="img/noticias/image-1.jpg" width="100%" height="100%">
-                    <p class="selecao-noticia">SELEÇÃO</p>
-                    <p class="conteudo-noticia">Kroos: ‘O Brasil nos mostrou que não somos tão bons quanto dizem’</p>
+                    <div class="col-md-3">
+                        <img src="img/noticias/image-1.jpg" width="100%" height="100%">
+                        <p class="selecao-noticia">SELEÇÃO</p>
+                        <p class="conteudo-noticia">Kroos: ‘O Brasil nos mostrou que não somos tão bons quanto dizem’</p>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
+    </div><!--Noticias-->
 
-    <div class="row">
-        <div id="rodape" class="col-md-12">
-            <div class="col-md-12">
-                <p>Fut Histórias</p>
-            </div>
-            <div class="col-md-4">
-                <h5>© Copyright 2018 - Disciplina Engenharia de Software II</h5>
-            </div>
-            <div class="col-md-4" id="menu-rodape">
-                <ul>
-                    <li><a href="#">Seleções</a> </li>
-                    <li><a href="#">Elencos</a> </li>
-                    <li><a href="#">Créditos</a> </li>
-                    <li><a href="#">Torcidômetro</a> </li>
-                </ul>
-            </div>
-            <div class="col-md-12">
-                <a href="#app"> <img src="img/seta.png" width="4%"> </a>
-            </div>
+
+    <div class="row" id="rodape">
+        <div class="container">
+            <h3>Fut Histórias</h3>
+            <h5> Tudo que você quer saber sobre as seleções da copa de 2018</h5>
+            <button onclick="topFunction()" id="myBtn" title="Voltar ao topo"><img src="img/seta_cima.png" width="30" height="30"></button>
         </div>
-    </div>
+    </div> <!--Rodape-->
 
 
 </div>
