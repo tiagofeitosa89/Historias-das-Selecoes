1c04ff6f0d130f2680d9c0964bca1b9ba9cb385e
Lucas Eduardo <lucasseduhh@gmail.com>
2018-04-29 18:48:19 -0300

    Alteracao informacoes do BD

--- Administrativo/_controle/conexao.php
+++ Administrativo/_controle/conexao.php
@@ -5,7 +5,7 @@
 		public static function getConexao(){
 			try{
 				if(empty($conn)){
-					$conn = new PDO('mysql:host=localhost;dbname=dados_copa2018','root', '');
+					$conn = new PDO('mysql:host=mysql762.umbler.com;dbname=histo_selecoes','masteradm', 'masteradm2904');
 					$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 				}
 				return $conn; 
