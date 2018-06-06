56f94dcb8862e0dbe8bf3830b7304027a5f03fa7
LukeEduh <lucasseduhh@gmail.com>
2018-06-05 22:14:59 -0300

    ajuste pagina sobre

--- sobre_nos.php
+++ sobre_nos.php
@@ -77,9 +77,9 @@
                         <h2 class="titulo-pagina">Nossa Equipe</h2>
                         <hr class="linha-titulo-pagina">
                   </div>  
-
                     <div id="featured" class="container">
-                    <div id="fbox1" class="class="p-3 mb-2 bg-success text-white""> <img src="img/sobre/lucas.jpg" width="150" height="150" alt=""/>
+                    <div class="col-md-3" id="colaboradores">
+                    <div id="equipe" class="class="p-3 mb-2 bg-success text-white""> <img src="img/sobre/lucas.jpg" width="150" height="150" alt=""/>
           <h2>Lucas Eduardo</h2>
           <p>Gerente de Projeto</p>
           <div class="rede" id="instan"> 
@@ -91,11 +91,12 @@
           <div class="rede" id="face"> 
         <a href="https://www.facebook.com/lukeeduh" target="_blank"><img class="icone" src="img/sobre/icones/face.png"></a>
           </div>
-          
         </div> 
+        </div>
     
-     
-      <div id="fbox2"> <img src="img/sobre/alberto.jpg" width="150" height="150" alt="" />
+      
+      <div class="col-md-3" id="colaboradores">
+      <div id="equipe"> <img src="img/sobre/alberto.jpg" width="150" height="150" alt="" />
           <h2>Luiz Alberto</h2>
           <p>Analista</p>
 
@@ -109,8 +110,10 @@
         <a href="https://www.facebook.com/Alberto0512" target="_blank"><img class="icone" src="img/sobre/icones/face.png"></a>
           </div>
       </div>
+      </div>
       
-      <div id="fbox3"> <img src="img/sobre/wedson.jpg" width="150" height="150" alt="" />
+      <div class="col-md-3" id="colaboradores">
+      <div id="equipe"> <img src="img/sobre/wedson.jpg" width="150" height="150" alt="" />
           <h2>Wedson Aguiar</h2>
           <p>Analista</p>
            <div class="rede" id="instan"> 
@@ -123,8 +126,10 @@
         <a href="https://www.facebook.com/sirwedson" target="_blank"><img class="icone" src="img/sobre/icones/face.png"></a>
           </div>
       </div>
-          
-      <div id="fbox4"> <img src="img/sobre/willer.jpg" width="150" height="150" alt="" />
+      </div>
+      
+      <div class="col-md-3" id="colaboradores">    
+      <div id="equipe"> <img src="img/sobre/willer.jpg" width="150" height="150" alt="" />
          <h2>Willer Alves</h2>
           <p>Desenvolvedor Front-End</p>
            <div class="rede" id="instan"> 
@@ -137,7 +142,13 @@
       <a href="https://www.facebook.com/willerallvees" target="_blank"><img class="icone" src="img/sobre/icones/face.png"></a>
           </div>
       </div>
-        <div id="fbox5"> <img src="img/sobre/jackson.jpg" width="150" height="150" alt="" />
+      </div>
+
+        <div class="col-md-2">
+        </div>
+
+        <div class="col-md-3" id="colaboradores_linha2">
+        <div id="equipe"> <img src="img/sobre/jackson.jpg" width="150" height="150" alt="" />
           <h2>Jackson Martins</h2>
           <p>Teste</p>
                 <div class="rede" id="instan"> 
@@ -150,7 +161,10 @@
       <a href="https://www.facebook.com/wjackson.martins" target="_blank"><img class="icone" src="img/sobre/icones/face.png"></a>
           </div>
       </div>
-         <div id="fbox6"> <img src="img/sobre/tiago.jpg" width="150" height="150" alt="" />
+      </div>
+
+        <div class="col-md-3" id="colaboradores_linha2">
+         <div id="equipe"> <img src="img/sobre/tiago.jpg" width="150" height="150" alt="" />
           <h2>TIAGO FEITOSA</h2>
           <p>Desenvolvedor Back-End</p>
                   <div class="rede" id="instan"> 
@@ -162,8 +176,11 @@
                   <div class="rede" id="face"> 
                       <a href="https://www.facebook.com/tiago.feitosa.75" target="_blank"><img class="icone" src="img/sobre/icones/face.png"></a>
                   </div>
+        </div>
       </div>
-          <div id="fbox7"> <img src="img/sobre/2.jpg" width="150" height="150" alt="" />
+
+        <div class="col-md-3" id="colaboradores_linha2">
+         <div id="equipe"> <img src="img/sobre/2.jpg" width="150" height="150" alt="" />
          <h2>Jefferson Moura</h2>
           <p>Teste</p>
           <div class="rede" id="instan"> 
@@ -175,8 +192,11 @@
                   <div class="rede" id="face"> 
                       <a href="http://www.facebook.com/jefersonmfreitas" target="_blank"><img class="icone" src="img/sobre/icones/face.png"></a>
                   </div>
-      </div>
-      </div>
+          </div>
+          </div>
+      </div> <!--fim da div container-->
+
+
       <div class="row" id="linha2"> <!--div colaboradores -->
         <div id="info" class="col-md-12"> <!--div titulo colaboradores -->
           <h2 class="titulo-pagina">Colaboradores</h2>
@@ -184,37 +204,55 @@
         </div> <!--fim da div titulo colaboradores --> 
 
                 <div id="featured" class="container">
-                    <div id="fbox1" class="colaboradores"p-3 mb-2 bg-success text-white""> <a href="https://medium.com/@365Esportes/latest" target="_blank"><img src="img/sobre/Colaboradores/365esportes.jpeg" width="150" height="150" alt=""/></a>
+                    <div class="col-md-3" id="colaboradores">
+                    <div id="fbox" p-3 mb-2 bg-success text-white""> <a href="https://medium.com/@365Esportes/latest" target="_blank"><img src="img/sobre/Colaboradores/365esportes.jpeg" width="150" height="150" alt=""/></a>
                     <h2>365Esportes</h2> 
-                    </div> 
+                    </div>
+                    </div>
 
-                    <div id="fbox2" class="colaboradores"p-3 mb-2 bg-success text-white""> <a href="http://www.futbox.com/pt" target="_blank"><img src="img/sobre/Colaboradores/futbox.png" width="150" height="150" alt=""/></a>
+                    <div class="col-md-3" id="colaboradores">
+                    <div id="fbox" p-3 mb-2 bg-success text-white""> <a href="http://www.futbox.com/pt" target="_blank"><img src="img/sobre/Colaboradores/futbox.png" width="150" height="150" alt=""/></a>
                     <h2>FUTBOX</h2> 
                     </div>
+                    </div>
 
-                    <div id="fbox3" class="colaboradores"p-3 mb-2 bg-success text-white""> <a href="http://esporte.ig.com.br/futebol/copa-do-mundo-2018/" target="_blank"><img src="img/sobre/Colaboradores/portal_ig.jpg" width="150" height="150" alt=""/></a>
+
+                    <div class="col-md-3" id="colaboradores">
+                    <div id="fbox" p-3 mb-2 bg-success text-white""> <a href="http://esporte.ig.com.br/futebol/copa-do-mundo-2018/" target="_blank"><img src="img/sobre/Colaboradores/portal_ig.jpg" width="150" height="150" alt=""/></a>
                     <h2>PORTAL IG</h2> 
                     </div>
+                    </div>
 
-                    <div id="fbox4" class="colaboradores"p-3 mb-2 bg-success text-white""> <a href="https://medium.com/@365Esportes/latest" target="_blank"><img src="img/sobre/Colaboradores/suapesquisa.gif" width="150" height="150" alt=""/></a>
+                    <div class="col-md-3" id="colaboradores">
+                    <div id="fbox" p-3 mb-2 bg-success text-white""> <a href="https://medium.com/@365Esportes/latest" target="_blank"><img src="img/sobre/Colaboradores/suapesquisa.gif" width="150" height="150" alt=""/></a>
                     <h2>SUAPESQUISA.COM</h2> 
                     </div>
+                    </div>
 
-                    <div id="fbox5" class="colaboradores"p-3 mb-2 bg-success text-white""> <a href="https://pt.wikipedia.org/wiki/Copa_do_Mundo_FIFA" target="_blank"><img src="img/sobre/Colaboradores/wikepedia.png" width="150" height="150" alt=""/></a>
+                    <div class="col-md-2">
+                    </div>  
+
+                    <div class="col-md-3" id="colaboradores_linha2">
+                    <div id="fbox" class="colaboradores"p-3 mb-2 bg-success text-white""> <a href="https://pt.wikipedia.org/wiki/Copa_do_Mundo_FIFA" target="_blank"><img src="img/sobre/Colaboradores/wikepedia.png" width="150" height="150" alt=""/></a>
                     <h2>WIKIPEDIA</h2> 
                     </div>
-
-                    <div id="fbox6" class="colaboradores"p-3 mb-2 bg-success text-white""> <a href="https://www.oddsshark.com/br/futebol/grupo-g-na-copa-do-mundo-2018-belgica-panama-tunisia-e-inglaterra" target="_blank"><img src="img/sobre/Colaboradores/odsshark.jpg" width="150" height="150" alt=""/></a>
-                    <h2>ODDSSHARK</h2> 
                     </div>
 
-                    <div id="fbox7" class="colaboradores"p-3 mb-2 bg-success text-white""> <a href="http://globoesporte.globo.com/futebol/copa-do-mundo/historia-das-copas.html" target="_blank"><img src="img/sobre/Colaboradores/globoesporte.png" width="150" height="150" alt=""/></a>
+                    <div class="col-md-3" id="colaboradores_linha2">
+                    <div id="fbox" class="colaboradores"p-3 mb-2 bg-success text-white""> <a href="https://www.oddsshark.com/br/futebol/grupo-g-na-copa-do-mundo-2018-belgica-panama-tunisia-e-inglaterra" target="_blank"><img src="img/sobre/Colaboradores/odsshark.jpg" width="150" height="150" alt=""/></a>
+                    <h2>ODDSSHARK</h2> 
+                    </div>
+                    </div>
+
+                    <div class="col-md-3" id="colaboradores_linha2">
+                    <div id="fbox" class="colaboradores"p-3 mb-2 bg-success text-white""> <a href="http://globoesporte.globo.com/futebol/copa-do-mundo/historia-das-copas.html" target="_blank"><img src="img/sobre/Colaboradores/globoesporte.png" width="150" height="150" alt=""/></a>
                     <h2>GLOBOESPORTE.COM</h2> 
-                    </div>     
+                    </div>
+                    </div>
                 </div>      
-      </div> <!--fim da div colaboradores --> 
-      </div> <!-- div corpo -->            
-    </div> <!--div row-->
+        </div> <!--fim da div colaboradores --> 
+      </div> <!-- div corpo -->    
+    </div> <!--div row-->          
   
     <div class="row" id="rodape">
         <div class="container">
